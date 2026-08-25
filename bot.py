822785f60205801d4d45b1d99c815e910adeead1
BungleBun <bunglebundev@gmail.com>
2026-08-25 12:01:26 -0700

    HOMER messaging function
    Added a working messaging function to HOMER.
    
    Tested my .env file to make sure no sensitive data is leaked.

---
+++ bot.py
@@ -0,0 +1,36 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+#TOKEN AND ID VARIABLES FROM ENV FILE
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+id = os.getenv("USER_ID")
+
+#BOT PERMISSIONS
+intents = discord.Intents.default()
+intents.message_content = True
+
+#CLIENT
+client = discord.Client(intents=intents)
+
+#FUNCTION TO TEST IF IT WORKS
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user.name}!")
+
+    try:
+
+        user = await client.fetch_user(id)
+        await user.send(f"Hello {user.name}! Homer is online!")
+
+    except discord.Forbidden:
+        print("Could not send message")
+
+    except Exception as e:
+        print(f"error occurred {e}.")
+
+if __name__ == "__main__":
+    client.run(token)
+
+
